43e3fcaae8d508942a8b96a3b14485363ecd6b9a
kumarabhiyadav <69810292+kumarabhiyadav@users.noreply.github.com>
2021-02-18 15:17:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <footer>
+       <!-- Add Containts hear Like About Us And Contacts -->
+       <div class="d-flex justify-content-between footer-contant p-4">
+        <h5>About Us</h5>
+        <h5> Help</h5>
+        <h5>Policy</h5>
+       
+       </div>
+
+    <hr>
+      <h4>&#169; Copyright</h4>
+    </footer>
+</body>
+</html>
